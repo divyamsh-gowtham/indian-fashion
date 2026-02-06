be2858328fb38fc989234d58d6babb7ebf1c2551
divyamsh-gowtham <146835070+divyamsh-gowtham@users.noreply.github.com>
2026-02-06 17:41:01 +0530

    Update page.tsx

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 "use client";
 
 import { useEffect, useState } from "react";
@@ -63,3 +64,4 @@ export default function ProfilePage() {
     </main>
   );
 }
+
